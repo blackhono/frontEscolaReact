568202b25840d0cfd0c9b457ea7ec6d20d8d21f1
blackhono <diegolar25@outlook.com>
2022-03-09 10:35:49 -0300

    modulo para users

---
+++ src/storeRedux/modules/user/reducer.js
@@ -0,0 +1,27 @@
+import * as types from '../types'
+// estados iniciais
+const initialState = {
+  user: null,
+  token: '',
+}
+
+export const loginReducer = (state, action) => {
+  switch (action.type) {
+    case types.LOGIN_SUCCESS: {
+      console.log('foi um sucessão! =) (Saga redux)')
+      return state
+    }
+    case types.LOGIN_REQUEST: {
+      console.log('estou fazendo a requisição (Saga redux)')
+      return state
+    }
+    case types.LOGIN_FAILURE: {
+      console.log('falhei em agir =( (Saga redux)')
+      return state
+    }
+
+    default: {
+      return state
+    }
+  }
+}
